352d2422a2179c5ddf163e84bf1e5dea4e8981b1
Blazkowich <oiluridze@gmail.com>
2024-11-14 12:53:56 +0400

    Minor fix ReviewDto

--- Ava.Application/Dtos/ReviewDto.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Ava.Application.Dtos;
-
-public record CreateReviewDto(Guid AuthorId, Guid RecipientId, int Rating, string Summary = "");
-public record UpdateReviewDto(Guid AuthorId, Guid RecipientId, int NewRating, string NewSummary = "");
--- Ava.Application/Dtos/Reviews.cs
+++ Ava.Application/Dtos/Reviews.cs
@@ -2,3 +2,5 @@
 
 public record ReviewDto(Guid Id, Guid AuthorId, Guid RecipientId, int Rating, string Summary);
 public record RatingSummary(double AverageRating, int TotalReviews);
+public record CreateReviewDto(Guid AuthorId, Guid RecipientId, int Rating, string Summary = "");
+public record UpdateReviewDto(Guid AuthorId, Guid RecipientId, int NewRating, string NewSummary = "");
